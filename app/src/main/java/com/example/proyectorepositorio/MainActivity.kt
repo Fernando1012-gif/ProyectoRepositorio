f0f4133d4f95eb46357eb8dbb55916671229aac8
adrim <adrimaru2005@gmail.com>
2025-10-31 13:26:05 -0600

    Si

--- app/src/main/java/com/example/proyectorepositorio/MainActivity.kt
+++ app/src/main/java/com/example/proyectorepositorio/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hola como estas???$name!",
+        text = "yutfiftid,fgmyt$name!",
         modifier = modifier
     )
 }
